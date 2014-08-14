84abeab4aae5c494d847688315b9cbe98c7ef331
Piotr Lewalski <piotr@lewalski.pl>
2014-08-14 21:15:35 +0200

    adding local environment file

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import os
 import django.core.management
+import sys
 import game.server
+import os.path
 
-with open(".env") as env:
+if os.path.isfile(".env_local"):
+    env_input = ".env_local"
+else:
+    env_input = ".env"
+
+with open(env_input) as env:
     for line in env.read().splitlines():
         var = line.split('=')
         print(var)
         os.environ.setdefault(*var)
 
-django.core.management.call_command('collectstatic', link=1, interactive=False)
+link = sys.platform != 'win32'
+django.core.management.call_command('collectstatic', link=link, interactive=False)
 game.server.main()
 
