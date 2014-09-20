85923d0a50b612221b2e31ebe0ba5e5c26d5b647
Piotr Lewalski <piotr@lewalski.pl>
2014-09-20 15:32:59 +0200

    adding initial data load

--- run.py
+++ run.py
@@ -27,5 +27,7 @@ import django.core.management
 link = sys.platform != 'win32'
 django.core.management.call_command('collectstatic', link=link, interactive=False)
 django.core.management.call_command('migrate', interactive=False)
+django.core.management.call_command('loaddata', 'game/apps/core/fixtures/initial_data.json',  interactive=False)
+
 game.server.main()
 
